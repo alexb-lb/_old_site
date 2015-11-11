849177b65a4d470a31760e19b201af58ab09af53
Alexandr Bobrenko <oknerbob@gmail.com>
2015-11-11 19:55:16 +0200

    edited mistage in closing tag. Time in js link cutter changed to 0

--- js/js.js
+++ js/js.js
@@ -166,7 +166,7 @@ function changeHref() {
         var newHref = loc.slice(0, newHrefEnds);
         setTimeout(function () {
             history.replaceState(null, null, newHref);
-        }, 40);
+        }, 0);
     }
 }
 
